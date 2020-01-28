475dd1cd76bbdea02c80cde13ed06cc25216903c
David Drvar <david.drvar.bogdanovic@gmail.com>
2020-01-28 11:15:31 +0100

    #parsiranje_skupa_HTML_dokumenata - dodato da svaki HTML dokument u grafu ima i listu onih koji imaju vezu ka njemu (incoming)

--- graph.py
+++ graph.py
@@ -3,40 +3,41 @@ from vertex import Vertex
 
 class Graph:
     def __init__(self):
-        self.vertList = {}
+        self.vert_list = {}
 
     def add_vertex(self, key):
-        newVertex = Vertex(key)
-        self.vertList[key] = newVertex
+        new_vertex = Vertex(key)
+        self.vert_list[key] = new_vertex
 
     def get_vertex(self, id):
-        if id in self.vertList:
-            return self.vertList[id]
+        if id in self.vert_list:
+            return self.vert_list[id]
         else:
             return None
 
     def __contains__(self, item): #checks if the vertex is in the graph
-        return item in self.vertList
+        return item in self.vert_list
 
     def add_edge(self, origin, destination, weight):
-        if origin not in self.vertList or destination not in self.vertList:
+        if origin not in self.vert_list or destination not in self.vert_list:
             raise ValueError("Vertices not in the graph")
-            return false
-        self.vertList[origin].add_neighbour(self.vertList[destination], weight)
+        self.vert_list[origin].add_neighbour(self.vert_list[destination], weight)
+        self.vert_list[destination].add_incoming(self.vert_list[origin],weight)
         return True
 
     def get_vertices(self):
-        return self.vertList.keys()
+        return self.vert_list.keys()
 
     def __iter__(self):
-        return iter(self.vertList.values())
+        return iter(self.vert_list.values())
 
     def insert(self,file,words,links):
         return self
 
     def print(self):
         for id in self.get_vertices():
-            t = tuple(f"{v.id}:{weight}" for v, weight in self.get_vertex(id).connectedTo.items())
-
-            print(f"edge: {id} to : {t}")
+            t1 = tuple(f"{v.id}:{weight}" for v, weight in self.get_vertex(id).outgoing.items())
+            t2 = tuple(f"{v.id}:{weight}" for v, weight in self.get_vertex(id).incoming.items())
+            print(f"vertex: {id} | outgoing : {t1} \n                   incoming : {t2}")
+            # print(f"edge: {id} to : {t}")
 
--- vertex.py
+++ vertex.py
@@ -1,19 +1,23 @@
 class Vertex:
     def __init__(self, key):
         self.id = key
-        self.connectedTo = {} # key is vertex object, value is weight
+        self.outgoing = {}  # key is vertex object, value is weight
+        self.incoming = {}
 
-    def add_neighbour (self, nbr, weight = None):
-        self.connectedTo[nbr] = weight
+    def add_neighbour(self, nbr, weight=None):
+        self.outgoing[nbr] = weight
+
+    def add_incoming(self, nbr, weight=None):
+        self.incoming[nbr] = weight
 
     def __str__(self):
-        return str(self.id) + 'connectedTo: ' + str([x.id for x in self.connectedTo])
+        return str(self.id) + 'connectedTo: ' + str([x.id for x in self.outgoing])
 
     def get_connections(self):
-        return self.connectedTo.keys()
+        return self.outgoing.keys()
 
     def get_id(self):
         return self.id
 
     def get_weight(self, nbr):
-        return self.connectedTo[nbr] #if it doesn't exist, it will return an error
+        return self.outgoing[nbr]  # if it doesn't exist, it will return an error
