e72010fcc54501af133a8e6f682f5151d6e735c7
David Drvar <david.drvar.bogdanovic@gmail.com>
2020-02-13 15:20:43 +0100

    #paginacija_rezultata : bug fix za beskonacnu petlju ukoliko je result_set prazan

--- pagination.py
+++ pagination.py
@@ -1,82 +1,83 @@
 # TODO David: Kada je result set prazan upadne u beskonacnu petlju sa pitanjem 'Enter numbers in range of 0 to 0'
 def pagination(result_set):
-    while True:
-        try:
-            page_length = int(input("Enter the page length: "))
+    if len(result_set) > 0:
+        while True:
+            try:
+                page_length = int(input("Enter the page length: "))
 
-            if page_length > len(result_set):
-                # print("Page length cannot be greater than ", len(result_set))
-                raise Exception
-            elif page_length <= 0:
-                # print("Page length must be greater than 0")
-                raise Exception
-            else:
+                if page_length > len(result_set):
+                    # print("Page length cannot be greater than ", len(result_set))
+                    raise Exception
+                elif page_length <= 0:
+                    # print("Page length must be greater than 0")
+                    raise Exception
+                else:
+                    break
                 break
-            break
-        except:
-            print("Enter only numbers in range of 1 to ", len(result_set))
+            except:
+                print("Enter only numbers in range of 1 to ", len(result_set))
 
-    index = 0
-    printable = result_set[index:page_length]
-    print()
-    if printable:
-        for (key, value) in printable:
-            line_new = '{:<150}   {:>15}'.format(key,value)
-            print(line_new)
-
-    index = page_length
-
-    while True:
+        index = 0
+        printable = result_set[index:page_length]
         print()
-        char = input(
-            "Enter A for the previous page, D for the next page and X for changing the page length, Q for exit:  ")
-        if char == 'X':
-            while True:
-                try:
-                    page_length = int(input("Enter the page length:  "))
+        if printable:
+            for (key, value) in printable:
+                line_new = '{:<150}   {:>15}'.format(key,value)
+                print(line_new)
 
-                    if page_length <= 0 or page_length> len(result_set):
-                        raise Exception
-                    else:
-                        index = 0
-                        printable = result_set[index:page_length]
-                        print()
-                        if printable:
-                            for (key,value) in printable:
-                                line_new = '{:<150}   {:>15}'.format(key, value)
-                                print(line_new)
-                        index = page_length
-                        break
-                except:
-                    print("Enter only numbers in range of 1 to ", len(result_set))
-        elif char == 'A':
-            tmp = index
-            index = index - 2 * page_length
+        index = page_length
+
+        while True:
             print()
-            printable = result_set[index: index + page_length]
-            if printable:
-                for (key, value) in printable:
-                    line_new = '{:<150}   {:>15}'.format(key, value)
-                    print(line_new)
-                index = index + page_length
+            char = input(
+                "Enter A for the previous page, D for the next page and X for changing the page length, Q for exit:  ")
+            if char == 'X':
+                while True:
+                    try:
+                        page_length = int(input("Enter the page length:  "))
+
+                        if page_length <= 0 or page_length> len(result_set):
+                            raise Exception
+                        else:
+                            index = 0
+                            printable = result_set[index:page_length]
+                            print()
+                            if printable:
+                                for (key,value) in printable:
+                                    line_new = '{:<150}   {:>15}'.format(key, value)
+                                    print(line_new)
+                            index = page_length
+                            break
+                    except:
+                        print("Enter only numbers in range of 1 to ", len(result_set))
+            elif char == 'A':
+                tmp = index
+                index = index - 2 * page_length
+                print()
+                printable = result_set[index: index + page_length]
+                if printable:
+                    for (key, value) in printable:
+                        line_new = '{:<150}   {:>15}'.format(key, value)
+                        print(line_new)
+                    index = index + page_length
+                else:
+                    print("There is no previous page")
+                    index = tmp
+
+
+            elif char == 'D':
+                printable = result_set[index: index + page_length]
+                print()
+                if printable:
+                    for (key, value) in printable:
+                        line_new = '{:<150}   {:>15}'.format(key, value)
+                        print(line_new)
+                    index = index + page_length
+                else:
+                    print("There is no next page")
+
+            elif char == 'Q':
+                break
+
             else:
-                print("There is no previous page")
-                index = tmp
-
-
-        elif char == 'D':
-            printable = result_set[index: index + page_length]
-            print()
-            if printable:
-                for (key, value) in printable:
-                    line_new = '{:<150}   {:>15}'.format(key, value)
-                    print(line_new)
-                index = index + page_length
-            else:
-                print("There is no next page")
-
-        elif char == 'Q':
-            break
-
-        else:
-            print("Only enter the aforementioned characters!")
+                print("Only enter the aforementioned characters!")
