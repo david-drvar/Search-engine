a4b0b3f121bc7b0b88b46e6499345886ee6f1e88
David Drvar <david.drvar.bogdanovic@gmail.com>
2020-02-21 15:43:57 +0100

    #evaluacija_stabla_parsiranja - pocetna verzija, '!' nije implementirano dobro

--- advanced_parsing.py
+++ advanced_parsing.py
@@ -1,5 +1,7 @@
+from my_set import MySet
 from parglare import Parser, Grammar
 from colors import Colors
+from query_parser import execute_query
 
 
 class OrNode:
@@ -65,9 +67,92 @@ actions = {
 }
 
 
-def make_ir(query):
-    #query = "!(java (python programming))"
+def evaluate_tree(node, trie, path):
+    # if node.right is None and node.left is None:
+    if isinstance(node, str): # list ce uvek biti str
+        return node
+    else:
+        left = evaluate_tree(node.left, trie, path)
+        right = evaluate_tree(node.right, trie, path)
+
+        if isinstance(node, OrNode):
+            if isinstance(left, str) and isinstance(right,str):
+                # criteria = left + 'OR' + right
+                criteria = []
+                criteria.append(left)
+                criteria.append('OR')
+                criteria.append(right)
+                result_set = execute_query(trie, criteria, path)
+
+            elif isinstance(left, MySet) and isinstance(right, MySet):
+                result_set = left.union(right)
+
+            elif isinstance(left,MySet) and isinstance(right,str):
+                criteria = right
+                result_right = execute_query(trie, criteria, path)
+                result_set = left.union(result_right)
+
+            elif isinstance(left, str) and isinstance(right,MySet):
+                criteria = left
+                result_left = execute_query(trie, criteria,path)
+                result_set = right.union(result_left)
+
+            return result_set
+
+        elif isinstance(node, AndNode): # oba str, oba setova, levo set i desno str, levo str i desno set
+            if isinstance(left, str) and isinstance(right,str):
+                # criteria = left + ' AND ' + right
+                criteria = []
+                criteria.append(left)  #java, AND, language MAX DVE RECI U KRITERIJUMU, nebitno and veliko ili malo
+                criteria.append('AND')
+                criteria.append(right)
+                result_set = execute_query(trie, criteria, path)
+
+            elif isinstance(left, MySet) and isinstance(right, MySet):
+                result_set = left.intersection(right)
+
+            elif isinstance(left,MySet) and isinstance(right,str):
+                criteria = right
+                result_right = execute_query(trie, criteria, path)
+                result_set = left.intersection(result_right)
+
+            elif isinstance(left, str) and isinstance(right,MySet):
+                criteria = left
+                result_left = execute_query(trie, criteria,path)
+                result_set = right.intersection(result_left)
+
+            return result_set
+
+        elif isinstance(node, NotNode): # fixme
+            if isinstance(right, str):
+                criteria = []
+                criteria.append('NOT')
+                criteria.append(right)
+                result_set = execute_query(trie, criteria, path)
+
+            elif isinstance(left, MySet) and isinstance(right, MySet):
+                result_set = left.difference(right)
+
+            elif isinstance(left, MySet) and isinstance(right, str):
+                criteria = right
+                result_right = execute_query(trie, criteria, path)
+                result_set = left.difference(result_right)
+
+            elif isinstance(left, str) and isinstance(right, MySet):
+                criteria = left
+                result_left = execute_query(trie, criteria, path)
+                result_set = result_left.difference(right)
+
+            return result_set
+
+
+def make_ir(query, trie, path):
+    # query = "!(java (python programming))"
+    query = "java || python || language"
     grammar = Grammar.from_file("bison_flex_grammar.txt")
     parser = Parser(grammar, actions=actions)
     ir_tree = parser.parse(query)
+    result_set = evaluate_tree(ir_tree, trie, path)
     print(ir_tree)
+
+    return result_set
